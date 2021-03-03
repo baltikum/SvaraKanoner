a59893dd8961ddcb79758ef4c2a7f15e6fdfe2c0
baltikum <mwdavidsson@outlook.com>
2021-03-03 20:43:12 +0100

    AllWords changed, new words added

--- server/src/server/AllWords.java
+++ server/src/server/AllWords.java
@@ -1,47 +1,64 @@
 package server;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Random;
+import java.util.*;
 
 /**
  *
  * Class to manage the Words of the game.
- * 20210212 baltikum
+ * 20210212 Mattias Davidsson
  */
 
 public class AllWords {
     private static File file;
     private static Random random;
-    private static ArrayList<String> allWords;
-
+    private static ArrayList<String> allWordsInGame;
+    private static HashSet<String> allWordsNoDuplicates;
 
     /**
      *
      * Constructor. Reads from file.
-     * "server/words/englishWords.txt"
+     * "server/words/en.txt"
      */
     public AllWords(){
 
-        this.allWords = new ArrayList<String>();
+        this.allWordsInGame = new ArrayList<>(700);
         this.file = new File("server/words/en.txt");
         this.random = new Random();
+        this.allWordsNoDuplicates = new HashSet<String>(700);
+        
+        if ( loadWordsFromFile(file) ) {
+            System.out.println("Words loaded.");
+        }
 
+    }
+
+    /**
+     * Used to load words from file into a ArrayList via HashSet.
+     * 
+     * @param file
+     * @return boolean
+     */
+    public boolean loadWordsFromFile(File file) {
         try (
-            InputStream inStream = new FileInputStream(file);
-            InputStreamReader inStreamReader = new InputStreamReader(inStream);
-            BufferedReader bufferedReader = new BufferedReader(inStreamReader);
-            ) {
+                InputStream inStream = new FileInputStream(file);
+                InputStreamReader inStreamReader = new InputStreamReader(inStream);
+                BufferedReader bufferedReader = new BufferedReader(inStreamReader);
+        ) {
 
             String line;
             while((line = bufferedReader.readLine()) != null) {
-                if (!checkForExistingWord(line)) {
-                    allWords.add(line);
-                }
+                    allWordsNoDuplicates.add(fixWord(line));
             }
+
+            for ( String str : allWordsNoDuplicates ) {
+                allWordsInGame.add(str);
+            }
+            
         } catch (Exception ex ){
-            System.out.println("allWords not loaded" + ex );
+            System.out.println("All words not loaded" + ex );
         }
+        return true;
     }
 
     /**
@@ -49,75 +66,54 @@ public class AllWords {
      * Function returns specified amount of random words.
      *
      * @param amount
-     * @return gameWords
+     * @return gameWords ArrayList
      */
     public  ArrayList<String> getWords(int amount) {
         ArrayList<String> gameWords = new ArrayList<>(amount);
 
         int index;
         for ( int i = 0; i<amount; i++ ) {
-            index = random.nextInt(allWords.size()-1);
-            gameWords.add(allWords.remove(index));
+            index = random.nextInt(allWordsInGame.size()-1);
+            gameWords.add(allWordsInGame.remove(index));
         }
         return gameWords;
     }
 
     /**
      *
-     * For developers use only. Adding new words into file.
+     * For developers use only. Adding new words into the game.
      * @param word
-     * @return
+     * @return boolean
      */
-    public String addWordToDictionary(String word ) {
-        String cleanWord = checkFixWord(word);
-        if ( !checkForExistingWord(cleanWord) ) {
-            try( BufferedWriter output =
+    public boolean addWordToDictionary(String word ) {
+        if ( word.matches(".*[A-Za-z].*")) {
+            String cleanWord = fixWord(word);
+                try( BufferedWriter output =
                          new BufferedWriter(
-                                 new FileWriter("server/words/englishWords.txt", true));
-            ) {
-                output.append(cleanWord + "\n");
-                return "Successfully added word.\n";
-            } catch (IOException ex){
-                return "Failed to add word.\n";
-            }
+                                 new FileWriter("server/words/en.txt", true));
+                ) {
+                    output.append(cleanWord + "\n");
+                    System.out.println("Successfully added word.\n");
+                    return true;
+                } catch (IOException ex){
+                    System.out.println("Failed to add word.\n");
+                    return false;
+                }
         }
-        return "Word already exists in file.\n";
+        System.out.println("Word already exists in file.\n");
+        return false;
     }
-
+    
     /**
      *
-     * Checks if word already exists.
+     * Trims deadspace and sets lower upper case letters.
      * @param word
-     * @return
+     * @return fixed String.
      */
-    private boolean checkForExistingWord(String word ) {
-        if ( allWords.contains(word) ) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    /**
-     *
-     * Controls only alphabetical letters,trims deadspace and Capitalizes initial letter.
-     * @param word
-     * @return
-     */
-    private String checkFixWord( String word ) {
-        if ( word == null ) {
-            return "NULL";
-        }
+    private String fixWord( String word ) {
         String trimmed = word.trim();
-        boolean checkCharacters = trimmed.matches(".*[A-Za-z].*");
-
-        if ( trimmed == "" ) {
-            return "Is empty.\n";
-        } else if ( !checkCharacters ) {
-            return "Only letters.\n";
-        }
-
-        String capital = (trimmed.substring(0, 1).toUpperCase() + trimmed.substring(1));
+        String capital = (trimmed.substring(0, 1).toUpperCase(Locale.ROOT) + trimmed.substring(1).toLowerCase(Locale.ROOT));
         return capital;
     }
+    
 }
